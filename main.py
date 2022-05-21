4b674a41815805eb821d472c5cc43f93e24c89fc
HelixCross <46133793+HelixCross@users.noreply.github.com>
2022-05-21 15:25:08 +0300

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from random import choice
-
-lst = ["Vampires", 'Lizardman', "TombKings", "Skaven", "High Elves", "Norsca", "Dwarves", "Empire"]
-
-print("The Army that you should learn using is the: " + choice(lst))
